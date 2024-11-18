43606ff35c0b0c8972d7bb431516e03bfbff366c
Abduazim1505 <80662455+Abduazim1505@users.noreply.github.com>
2024-11-18 10:15:40 +0500

    добавление centerStructure

--- src/components/centerStructure/Structure/index.jsx
+++ src/components/centerStructure/Structure/index.jsx
@@ -1,15 +1,33 @@
-import styles from './Structure.module.css'
+import styles from './Structure.module.css';
+import classN from 'classnames';
 
 import Card from '../Card';
+import СircularShadow from "@/components/СircularShadow";
 
 function Structure(props) {
     return (
-        <header className='container'>
-            <div className={styles.director}>
-                <Card />
-            </div>
-            <Card />
-        </header>
+        <>
+            <header className={classN('container', styles.wrap)}>
+                <h2 className={classN('sectionTitle', styles.title)}>Markaz TUZILMASI</h2>
+                <div className={styles.wrpa_director}>
+                    
+                </div>
+
+                <div className={styles.director}>
+                        <Card />
+                    </div>
+
+                <div className={styles.grid}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                <СircularShadow className={styles.circular_min} />
+                <СircularShadow className={styles.circular} />
+            </header>
+            <p className={styles.background_text}>MarkazTUZILMASI</p>
+        </>
+
     );
 }
 
