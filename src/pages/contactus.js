7b1b87b4b3a7a7372e5711d7c7a8257846c06057
bohodir <bohodir29102001@gmail.com>
2024-11-29 16:18:22 +0500

    done

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,22 +1,37 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import Contact from '@/components/contatUsPage/ContactUs'
+import Contact from "@/components/contatUsPage/ContactUs";
+import { axiosT } from "@/api/axios";
 
 export async function getServerSideProps({ locale }) {
-    return {
-      props: {
-        ... (await serverSideTranslations(locale, ['common',])),
-        locale: locale
-      },
-    };
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      locale: locale,
+    },
+  };
+}
 
 function Contactus(props) {
-    return (
-        <>
-            <Contact/>
-        </>
-    );
+  useEffect(() => {
+    axiosT
+      .get("/service", {
+        headers: {
+          "Accept-Language": locale,
+        },
+      })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((er) => {
+        console.log(er);
+      });
+  }, []);
+  return (
+    <>
+      <Contact />
+    </>
+  );
 }
 
 export default Contactus;
