bf9716b4eaf3b11dccba561867779fbbb41d842a
bohodir <bohodir29102001@gmail.com>
2024-11-29 17:44:09 +0500

    done

--- src/api/pagesApis/contactUs.js
+++ src/api/pagesApis/contactUs.js
@@ -1,6 +1,26 @@
 import { axiosT } from "../axios";
 
+function getCsrfToken() {
+  const cookies = document.cookie.split(";");
+  for (const cookie of cookies) {
+    const [key, value] = cookie.trim().split("=");
+    if (key === "csrftoken") {
+      return value;
+    }
+  }
+  return null; // Return null if the token is not found
+}
 export const postServiceMessage = async (payload) => {
-  const { data } = await axiosT.post("/service/message", payload);
+  const csrfToken = getCsrfToken();
+
+  if (!csrfToken) {
+    console.error("CSRF token not found");
+    return;
+  }
+  const { data } = await axiosT.post("/service/message", payload, {
+    headers: {
+      "X-CSRF-Token": csrfToken,
+    },
+  });
   return data;
 };
