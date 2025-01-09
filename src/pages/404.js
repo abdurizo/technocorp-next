90d8c122e7e1f31570c81656db612f5fd8a05b76
bohodir <bohodir29102001@gmail.com>
2025-01-09 14:10:07 +0500

    done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,27 @@
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      locale: locale,
+    },
+  };
+}
 
 export default function Custom404() {
+  const { t } = useTranslation();
   return (
     <>
       <div className="wrap_404">
-        <h1>404 - Страница не найдена</h1>
-        <p>К сожалению, мы не смогли найти запрашиваемую страницу.</p>
-        <a href="/" className="btn_404">
-          Вернуться на главную
-        </a>
+        <h1 className="text-[200px]">404</h1>
+        <p className="text-[40px] mb-10">{t("not_found")}</p>
+        <Link href="/" className="btn_404">
+          {t("back_main")}
+        </Link>
       </div>
-
     </>
   );
 }
